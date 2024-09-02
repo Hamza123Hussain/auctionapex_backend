0846ed9013a0e4feb11f02a637eb175d566e1c28
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-03 04:49:03 +0500

    2 paths of auth router made

--- DB/Router/AuthRouter.js
+++ DB/Router/AuthRouter.js
@@ -1,13 +1,10 @@
-import multer from 'multer'
 import express from 'express'
-import { Register } from '../Controllers/Auth/Register.js'
+import { Signin } from '../Controllers/Auth/Register.js'
+import { upload } from '../Middleware/Multer.js'
 import { Login } from '../Controllers/Auth/Login.js'
-import { Signout } from '../Controllers/Auth/Signout.js'
-import ResetPass from '../Controllers/Auth/ResetPass.js'
-const upload = multer({ storage: multer.memoryStorage() })
 const AuthRouter = express.Router()
-AuthRouter.post('/Register', upload.single('File'), Register)
+AuthRouter.post('/SignIn', upload.single('image'), Signin)
 AuthRouter.post('/Login', Login)
-AuthRouter.get('/SignOut', Signout)
-AuthRouter.post('/Reset', ResetPass)
+// AuthRouter.get('/SignOut', Signout)
+// AuthRouter.post('/Reset', ResetPass)
 export default AuthRouter
