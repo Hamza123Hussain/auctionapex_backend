15ebf210e277c5aebe5b00ecb18158c7d964ebeb
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-03 04:50:23 +0500

    basic auth controllers made

--- DB/Controllers/Auth/Login.js
+++ DB/Controllers/Auth/Login.js
@@ -1,19 +1,44 @@
-import { signInWithEmailAndPassword } from 'firebase/auth'
-import { auth } from '../../../FirebaseConfig.js'
+import { generateToken } from '../../../JsonWebToken.js'
+import User from '../../Model/User.js'
+import bcrypt from 'bcrypt'
 
 export const Login = async (req, res) => {
-  const { inputVal } = req.body
+  const { email, password } = req.body
+
+  // Check if both email and password are provided
+  if (!email || !password) {
+    return res.status(400).json({ message: 'Email and password are required' })
+  }
+
   try {
-    const userCredential = await signInWithEmailAndPassword(
-      auth,
-      inputVal.email,
-      inputVal.password
-    )
-    if (userCredential) {
-      res.status(200).json(userCredential.user.uid)
+    // Find the user by email
+    const findUser = await User.findOne({ email })
+
+    if (!findUser) {
+      return res.status(404).json({ message: 'User does not exist' })
     }
+
+    // Compare provided password with stored hashed password
+    const matchPassword = await bcrypt.compare(password, findUser.password)
+
+    if (!matchPassword) {
+      return res.status(400).json({ message: 'Password does not match' })
+    }
+
+    // Generate JWT token
+    const token = generateToken(findUser._id)
+
+    // Respond with token and user data
+    res.status(200).json({
+      token,
+      user: {
+        id: findUser._id,
+        name: findUser.username,
+        email: findUser.email,
+        image: findUser.image,
+      },
+    })
   } catch (error) {
-    console.error('Error during Login:', error)
-    res.status(500).send('Error during Login')
+    res.status(500).json({ message: 'Server error', error })
   }
 }
