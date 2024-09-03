eafd8773e750ac3a34935e3512793349df46558a
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-03 21:01:35 +0500

    chnaged data type

--- DB/Model/Product.js
+++ DB/Model/Product.js
@@ -1,28 +1,24 @@
 import mongoose from 'mongoose'
 
 const ProductSchema = new mongoose.Schema({
-  _id: { type: String }, // Use String type for _id
+  _id: { type: String }, // Use String type for _id or change to ObjectId if needed
   productName: { type: String, required: true }, // Name of the product
   description: { type: String }, // Description of the product
   price: { type: Number, required: true }, // Starting price for the auction
   image: { type: String }, // URL or path to the product image
-  //   category: { type: String }, // Category of the product
-  auctionEndDate: { type: String, required: true }, // Date and time when the auction ends
+  auctionEndDate: { type: Date, required: true }, // Date and time when the auction ends
   bids: [
     {
-      // Array to store bids placed on the product
-      userId: { type: String, ref: 'users' },
+      userId: { type: String }, // Assuming userId is stored as a String
       bidAmount: { type: Number },
       bidTime: { type: Date, default: Date.now },
     },
   ],
   sellerId: {
-    type: String,
-    ref: 'users',
+    type: String, // Assuming sellerId is stored as a String
     required: true,
   }, // Seller's ID
   status: {
-    // Status of the product (e.g., active, sold, canceled)
     type: String,
     enum: ['active', 'sold', 'canceled'],
     default: 'active',
