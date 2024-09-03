caaffc6c5d4df4258e96adf3e02fde7f1bc722af
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-03 06:36:21 +0500

    added some new details

--- DB/Model/Product.js
+++ DB/Model/Product.js
@@ -7,12 +7,19 @@ const ProductSchema = new mongoose.Schema({
   image: { type: String }, // URL or path to the product image
   //   category: { type: String }, // Category of the product
   auctionEndDate: { type: String, required: true }, // Date and time when the auction ends
-  //   bids: [{ // Array to store bids placed on the product
-  //     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  //     bidAmount: { type: Number },
-  //     bidTime: { type: Date, default: Date.now }
-  //   }],
-  //   sellerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }, // Seller's ID
+  bids: [
+    {
+      // Array to store bids placed on the product
+      userId: { type: mongoose.Schema.Types.ObjectId, ref: 'users' },
+      bidAmount: { type: Number },
+      bidTime: { type: Date, default: Date.now },
+    },
+  ],
+  sellerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'users',
+    required: true,
+  }, // Seller's ID
   status: {
     // Status of the product (e.g., active, sold, canceled)
     type: String,
