5150f129f7a997bead582443afeb777bbf3a26ff
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-05 01:28:50 +0500

    enabled signout api

--- DB/Controllers/Auth/Signout.js
+++ DB/Controllers/Auth/Signout.js
@@ -4,7 +4,7 @@ import { Auth } from '../../../FireBaseConfig.js'
 export const Signout = async (req, res) => {
   try {
     await signOut(Auth)
-    res.status(200).json({ message: 'Sign-out successful' })
+    res.status(200).json(true)
   } catch (error) {
     console.error('Error during signout:', error)
     res.status(500).send('Error during signout')
