c99d169e7794a93d35096ca47999c4d61038076f
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-06 02:23:08 +0500

    added another route to get all the auctions made

--- DB/Router/AuctionRouter.js
+++ DB/Router/AuctionRouter.js
@@ -1,5 +1,7 @@
 import { Router } from 'express'
 import { MakeAuction } from '../Controllers/Auction/CreateAuction.js'
+import { getAllAuctions } from '../Controllers/Auction/GetAllAuctions.js'
 const AuctionRouter = Router()
 AuctionRouter.post('/MakeAuction', MakeAuction)
+AuctionRouter.get('/AllAuctions', getAllAuctions)
 export default AuctionRouter
