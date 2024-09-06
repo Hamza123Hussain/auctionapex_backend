988f36a3043aa0d39c33a4a2cf37a5299b33c077
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-06 20:31:38 +0500

    added the bid placing path

--- DB/Router/AuctionRouter.js
+++ DB/Router/AuctionRouter.js
@@ -2,9 +2,11 @@ import { Router } from 'express'
 import { MakeAuction } from '../Controllers/Auction/CreateAuction.js'
 import { getAllAuctions } from '../Controllers/Auction/GetAllAuctions.js'
 import { getSingleAuction } from '../Controllers/Auction/GetSingleAuction.js'
+import { PlaceBid } from '../Controllers/Auction/NewBid.js'
 const AuctionRouter = Router()
 AuctionRouter.post('/MakeAuction', MakeAuction)
 AuctionRouter.get('/AllAuctions', getAllAuctions)
 AuctionRouter.get('/SingleAuction', getSingleAuction)
 /**give AuctionID as query */
+AuctionRouter.post('/SingleBid', PlaceBid)
 export default AuctionRouter
