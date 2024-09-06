4f2b1990917766c728f3fd732a4084f6f78c2b03
Hamza Hussain <87210198+Hamza123Hussain@users.noreply.github.com>
2024-09-07 04:05:34 +0500

    Update AuctionRouter.js

--- DB/Router/AuctionRouter.js
+++ DB/Router/AuctionRouter.js
@@ -5,8 +5,8 @@ import { getSingleAuction } from '../Controllers/Auction/GetSingleAuction.js'
 import { PlaceBid } from '../Controllers/Auction/NewBid.js'
 const AuctionRouter = Router()
 AuctionRouter.post('/MakeAuction', MakeAuction)
-AuctionRouter.get('/AllAuctions', getAllAuctions)
-AuctionRouter.get('/SingleAuction', getSingleAuction)
+// AuctionRouter.get('/AllAuctions', getAllAuctions)
+// AuctionRouter.get('/SingleAuction', getSingleAuction)
 /**give AuctionID as query */
 AuctionRouter.post('/SingleBid', PlaceBid)
 export default AuctionRouter
